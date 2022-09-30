3a65a1e540147262e0abd72b3903dacfd9d76ee9
aprilianingsih <aprilianingsih954@gmail.com>
2022-09-30 13:00:33 +0700

    mengupload file tugas ke repo github

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ['index.html'],
+    theme: {
+        container:{
+            center: true,
+            padding: '16xp',
+        },
+        extend:{},
+    },
+    screens: {
+        '2xl':'1320px'
+    },
+    plugins:[],
+};
